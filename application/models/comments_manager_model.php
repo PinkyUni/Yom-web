5cadc7840a0f5a9d8aab0f186a31c4fcee6c3866
PinkyUni <30stmars6277@gmail.com>
2019-05-08 01:41:26 +0300

    fixed bugs

--- application/models/comments_manager_model.php
+++ application/models/comments_manager_model.php
@@ -34,6 +34,8 @@ class Comments_Manager_Model extends Model
         foreach ($_POST['comments'] as $comment) {
             $query = "UPDATE comments SET accepted = 1 WHERE id = $comment;";
             $mySQLConnector->executeQuery($query);
+
+            $this->send_email($comment);
         }
     }
 
@@ -46,6 +48,48 @@ class Comments_Manager_Model extends Model
         foreach ($_POST['comments'] as $comment) {
             $query = "DELETE FROM comments WHERE id = $comment;";
             $mySQLConnector->executeQuery($query);
+        }
+    }
+
+    private function send_email($id)
+    {
+        require_once 'mysqlconnector.php';
+
+        $mySQLConnector = MySQLConnector::getInstance();
+
+        $query = "SELECT * FROM comments WHERE id = $id;";
+        $comment_info = $mySQLConnector->getQueryResult($query);
+
+        $rec_id = $comment_info[0]['recipe_id'];
+
+        $query = "SELECT username FROM recipes WHERE id = $rec_id;";
+        $username = $mySQLConnector->getSingleValue($query,'username');
+
+        $query = "SELECT * FROM users WHERE name = '$username';";
+        $user_info = $mySQLConnector->getQueryResult($query);
+        $subscribed = $user_info[0]['subscribed'];
+
+        if (strcmp($subscribed, 'yes') == 0) {
+
+            $email = $user_info[0]['email'];
+            $text = $comment_info[0]['text'];
+
+            $subject = "New comment!";
+
+            $link = "yom.com/recipe/$username/$id";
+            $text = str_replace("\n", "<br>", $text);
+
+            $message = '<section style="margin: 2vw 30%;padding: 2vw;text-align: center;border-radius: 5px;border: 1px solid #9e9e9e;font-family: Georgia, serif;color: #585858;">
+            <nav><h1 style="margin: 0;padding: 0.5em 0;font-size: 3em;">Yom</h1></nav><hr style="width: 100%;"><article><h2 style="color: #edb025;">You have a new comment!</h2>
+            <h3 style="color: #585858;">' . $username . ' posted a new comment under your recipe:</h3><div style="text-align: left;padding: 1vw;margin-bottom: 1vw;border: 1px solid #cccccc;border-radius: 10px;">
+        ' . $text . '</div><div>Check <a href=' . $link . ' style="color: #e2b03f;">here.</a></div></article></section>';
+
+            $headers = "From: Yom.com <yom.com.recipes@gmail.com>\r\n";
+            $headers .= "Reply-To: keklolpukger@gmail.com\r\n";
+            $headers .= "MIME-Version: 1.0\r\n";
+            $headers .= "Content-type: text/html; charset=windows-1251 \r\n";
+
+            $res = mail($email, $subject, $message, $headers);
         }
     }
 }
--- application/models/recipe_model.php
+++ application/models/recipe_model.php
@@ -69,48 +69,11 @@ class Recipe_Model extends Model
                 $res = $mySQLConnector->executeQuery($query);
 
                 if ($res) {
-                    $this->send_email($id, $text);
                     $this->mail_admin();
                 }
             }
         } else {
             header("Location: /login");
-        }
-    }
-
-    private function send_email($id, $text)
-    {
-        require_once 'mysqlconnector.php';
-
-        $mySQLConnector = MySQLConnector::getInstance();
-
-        $query = "SELECT username FROM recipes WHERE id = $id;";
-        $username = $mySQLConnector->getSingleValue($query, 'username');
-
-        $query = "SELECT subscribed FROM users WHERE name = '$username';";
-        $subscribed = $mySQLConnector->getSingleValue($query, 'subscribed');
-
-        if (strcmp($subscribed, 'yes') == 0) {
-
-            $query = "SELECT email FROM users WHERE name = '$username';";
-            $email = $mySQLConnector->getSingleValue($query, 'email');
-
-            $subject = "New comment!";
-
-            $link = "yom.com/recipe/$username/$id";
-            $text = str_replace("\n", "<br>", $text);
-
-            $message = '<section style="margin: 2vw 30%;padding: 2vw;text-align: center;border-radius: 5px;border: 1px solid #9e9e9e;font-family: Georgia, serif;color: #585858;">
-            <nav><h1 style="margin: 0;padding: 0.5em 0;font-size: 3em;">Yom</h1></nav><hr style="width: 100%;"><article><h2 style="color: #edb025;">You have a new comment!</h2>
-            <h3 style="color: #585858;">' . $username . ' posted a new comment under your recipe:</h3><div style="text-align: left;padding: 1vw;margin-bottom: 1vw;border: 1px solid #cccccc;border-radius: 10px;">
-        ' . $text . '</div><div>Check <a href=' . $link . ' style="color: #e2b03f;">here.</a></div></article></section>';
-
-            $headers = "From: Yom.com <yom.com.recipes@gmail.com>\r\n";
-            $headers .= "Reply-To: keklolpukger@gmail.com\r\n";
-            $headers .= "MIME-Version: 1.0\r\n";
-            $headers .= "Content-type: text/html; charset=windows-1251 \r\n";
-
-            $res = mail($email, $subject, $message, $headers);
         }
     }
 
@@ -131,7 +94,7 @@ class Recipe_Model extends Model
             $headers .= "MIME-Version: 1.0\r\n";
             $headers .= "Content-type: text/html; charset=windows-1251 \r\n";
 
-            $res = mail($email, $subject, $message, $headers);
+            mail($email, $subject, $message, $headers);
     }
 
     public function get_comments($id)
