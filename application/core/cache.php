358fd75385bdee36d7c6b756f876d992cd40af8e
PinkyUni <30stmars6277@gmail.com>
2019-05-23 09:02:54 +0300

    Fixed caching

--- application/core/cache.php
+++ application/core/cache.php
@@ -1,25 +1,35 @@
 <?php
 
-class Cache {
+class Cache
+{
 
     private $cache_file = '';
     private $cache_time = 60;
 
-    private function get_cache_file() {
+    private function get_cache_file()
+    {
         return $this->cache_file;
     }
 
-    private function set_cache_file($cache_file) {
+    private function set_cache_file($cache_file)
+    {
         $this->cache_file = $cache_file;
     }
 
-    public function read_cache() {
-        $file = 'main';
-        $links = explode('/', $_SERVER['REQUEST_URI']);
-        if (!empty($links[1])) {
-            $file = $links[1];
-        }
-        $cache_file = "application/cache/$file.html";
+    public function read_cache()
+    {
+        if (strcmp($_SERVER['REQUEST_URI'], '/') == 0)
+            $file = '/main';
+        else
+            $file = $_SERVER['REQUEST_URI'];
+//
+//        $links = explode('/', $_SERVER['REQUEST_URI']);
+//        if (!empty($links[1])) {
+//            $file = $links[1];
+//        }
+        if (isset($_SESSION['session_username']))
+            $file .= '_' . $_SESSION['session_username'];
+        $cache_file = "application/cache$file.html";
         $this->set_cache_file($cache_file);
         if (file_exists($cache_file)) {
             if ((time() - $this->cache_time) < filemtime($cache_file)) {
@@ -30,7 +40,8 @@ class Cache {
         ob_start();
     }
 
-    public function write_cache() {
+    public function write_cache()
+    {
         $cache_file = $this->get_cache_file();
         $handle = fopen($cache_file, 'w');
         fwrite($handle, ob_get_contents());
